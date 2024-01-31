96c163b6de46433800d004c2a2fdb26d80aaf6f7
AlexLiCa <alexlin.carre@gmail.com>
2024-01-31 14:32:44 -0600

    -Captura de datos en pagina

--- app.py
+++ app.py
@@ -1,23 +1,80 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import pymongo
+# Asegúrate de importar tu clase Cuenta desde el módulo correspondiente
+from cuenta import Cuenta
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def transferencia():
-    return render_template('index.html')
+def obtener_coleccion():
+    cliente = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = cliente["banco_distribuidos"]
+    return db["Cuentas"]
 
-@app.route('/procesar_formulario', methods=['POST'])
-def procesar_formulario():
-    if request.method == 'POST':
-        cuenta_origen = request.form['cuenta_origen']
-        cuenta_destino = request.form['cuenta_destino']
-        cuenta_monto = request.form['monto']
-        
-        # Puedes hacer lo que quieras con los datos, por ejemplo, imprimirlos en la consola
-        print(f'Cuenta Origen: {cuenta_origen}, Cuenta destino: {cuenta_destino}')
-        # También puedes redirigir a otra página o mostrar un mensaje de éxito
-        return '¡Formulario enviado correctamente!'
+
+@app.route('/')
+def inicio():
+    return render_template('inicio.html')
+
+@app.route('/transferencia')
+def transferencia():
+    return render_template('transferencia.html')
+
+
+@app.route('/retirar')
+def retirar():
+    return render_template('retirar.html')
+
+@app.route('/depositar')
+def depositar():
+    return render_template('depositar.html')
+
+@app.route('/trans', methods=['POST'])
+def trans():
+    try:
+        titular = request.form['titular']
+        no_cuenta = int(request.form['no_cuenta'])
+        cantidad = float(request.form['cantidad'])
+    except:
+        print("No se pusieron datos validos")
+
+    print(
+        f"Titular: {titular}: \n Cuenta: {no_cuenta}.  \nMonto: {cantidad}"
+        )
+    
+    return redirect(url_for('inicio'))
+
+
+@app.route('/retiros', methods=['POST'])
+def retiros():
+    try:
+        titular = request.form['titular']
+        no_cuenta = int(request.form['no_cuenta'])
+        cantidad = float(request.form['cantidad'])
+    except:
+        print("No se pusieron datos validos")
+
+    print(
+        f"Titular: {titular}: \nCuenta: {no_cuenta}.\nMonto: {cantidad}"
+    )
+
+    return redirect(url_for('inicio'))
+
+
+@app.route('/depositos', methods=['POST'])
+def depositos():
+    try:
+        titular = request.form['titular']
+        no_cuenta = int(request.form['no_cuenta'])
+        cantidad = float(request.form['cantidad'])
+    except:
+        print("No se pusieron datos validos")
+
+    print(
+        f"Titular: {titular}: \nCuenta: {no_cuenta}.  \nMonto:{cantidad}"
+    )
+
+    return redirect(url_for('inicio'))
 
 
 if __name__ == '__main__':
